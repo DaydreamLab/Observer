d735a0412e7d3811e7da2e582e3898835bd63020
Jordan <jordan@daydream-lab.com>
2019-10-03 12:32:07 +0800

    update

--- src/Controllers/Search/Admin/SearchAdminController.php
+++ src/Controllers/Search/Admin/SearchAdminController.php
@@ -22,82 +22,9 @@ class SearchAdminController extends BaseController
     }
 
 
-    public function getItem($id)
-    {
-        $this->service->getItem($id);
-
-        return ResponseHelper::response($this->service->status, $this->service->response);
-    }
-
-
-    public function getItems()
-    {
-        $this->service->search(new Collection());
-
-        return ResponseHelper::response($this->service->status, $this->service->response);
-    }
-
-
-    public function getList()
-    {
-        $this->service->getList(new Collection());
-
-        return ResponseHelper::response($this->service->status, $this->service->response);
-    }
-
-
-    public function checkout($id)
-    {
-        $this->service->checkout($id);
-
-        return ResponseHelper::response($this->service->status, $this->service->response);
-    }
-
-
-    public function ordering(SearchAdminOrderingPost $request)
-    {
-        $this->service->ordering($request->rulesInput());
-
-        return ResponseHelper::response($this->service->status, $this->service->response);
-    }
-
-
-    public function remove(SearchAdminRemovePost $request)
-    {
-        $this->service->remove($request->rulesInput());
-
-        return ResponseHelper::response($this->service->status, $this->service->response);
-    }
-
-
-    public function state(SearchAdminStatePost $request)
-    {
-        $this->service->state($request->rulesInput());
-
-        return ResponseHelper::response($this->service->status, $this->service->response);
-    }
-
-
-    public function store(SearchAdminStorePost $request)
-    {
-        $this->service->store($request->rulesInput());
-
-        return ResponseHelper::response($this->service->status, $this->service->response);
-    }
-
-
-    public function search(SearchAdminSearchPost $request)
-    {
-        $input = $request->rulesInput();
-        $input->put('page', $request->has('page') ? $request->get('page') : 1);
-
-        $this->service->search($input);
-
-        return ResponseHelper::response($this->service->status, $this->service->response);
-    }
-
     public function keywordList(SearchAdminKeywordListPost $request)
     {
+        $this->service->canAction('searchKetwordList');
         $this->service->keywordList($request->rulesInput());
         
         return ResponseHelper::response($this->service->status, $this->service->response);
